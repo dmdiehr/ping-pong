c426a26104fc12a4c91756cb53dff116ea4cedd1
dmdiehr <ddiehr@yahoo.com>
2015-10-19 15:08:00 -0700

    Add recursive callback functions to sync center div animation.

--- js/improved.js
+++ js/improved.js
@@ -4,38 +4,108 @@ var pongInput;
 var gameLength;
 var leftScore = 0;
 var rightScore = 0;
-var leftPaddle;
-var rightPaddle;
-var leftLag = 0;
-var centerLag = 0;
-var rightLag = 0;
+var counter = 0;
 
-//functions to control div animation
 
-//center div
+////////////////////////functions to control div animation//////////////////////////////////////
+
 function countDown(callback){
   $('#countdown3').fadeIn(1000).delay(500).fadeOut(500, function(){
     $('#countdown2').fadeIn(1000).delay(500).fadeOut(500, function(){
       $('#countdown1').fadeIn(1000).delay(500).fadeOut(500, function(){
         $('#countdown-start').fadeIn(1000).delay(500).fadeOut(100, function(){
-          callback();
+          if (callback){
+            // console.log('callback exists');
+            callback(pingInput, pongInput, gameLength);
+          }
         });//countdown-start ending
       });//countdown1 ending
     });//countdown2 ending
   });//countdown3 ending
 }//countDown function ending
 
+////////////////////////////////////////center div////////////////////////////////////////////////
 
-function motionLeft(){
-  $('#left-motion').fadeIn( 500 ).delay( 500 ).fadeOut( 500 );
+function motionLeft(callback){
+  $('#left-motion').fadeIn(500).delay(500).fadeOut(500, function(){
+    if (callback){
+      counter++;
+      console.log('motionLeft ' + counter)
+      if (counter<gameLength){
+        callback(motionLeft);
+      } else {
+        callback();
+      }
+    }
+  });
 }
-function motionRight(){
-  $('#right-motion').fadeIn( 500 ).delay( 500 ).fadeOut( 500 );
+function motionRight(callback){
+  $('#right-motion').fadeIn( 500 ).delay(500).fadeOut(500, function(){
+    if (callback){
+      counter++;
+      console.log('motionRight '+ counter)
+      if (counter<gameLength){
+        callback(motionRight);
+      } else {
+        callback();
+      }
+    }
+  });
 }
 
+// function centerAnimationLoop(length){
+//     motionLeft(motionRight);
+//   }
+
+//////////////////////////////////left div/////////////////////////////////////////////////////
+
+function leftNumber(callback){
+  $('#left-number').fadeIn(500).delay(500).fadeOut(500, function(){
+    if (callback){callback();}
+  });
+}
+
+function leftPing(callback){
+  $('#left-ping').fadeIn(500).delay(500).fadeOut(500, function(){
+    if (callback){callback();}
+  });
+}
+function leftPong(callback){
+  $('#left-pong').fadeIn(500).delay(500).fadeOut(500, function(){
+    if (callback){callback();}
+  });
+}
+function leftPongPong(callback){
+  $('#left-ping-pong').fadeIn(500).delay(500).fadeOut(500, function(){
+    if (callback){callback();}
+  });
+}
+//////////////////////////////////// right div////////////////////////////////////////////////////
+function rightNumber(callback){
+  $('#right-number').fadeIn(500).delay(500).fadeOut(500, function(){
+    if (callback){callback();}
+  });
+}
+
+function rightPing(callback){
+  $('#right-ping').fadeIn(500).delay(500).fadeOut(500, function(){
+    if (callback){callback();}
+  });
+}
+function rightPong(callback){
+  $('#right-pong').fadeIn(500).delay(500).fadeOut(500, function(){
+    if (callback){callback();}
+  });
+}
+function righttPongPong(callback){
+  $('#right-ping-pong').fadeIn(500).delay(500).fadeOut(500, function(){
+    if (callback){callback();}
+  });
+}
 
 
 function leftPlayerTurn(output){
+  console.log('left player ' + output);
   
   //if output = ping - show ping img, add 1 point
 
@@ -46,11 +116,11 @@ function leftPlayerTurn(output){
   //if output = number - show number
   
   // show left-ball img
-  // $(this).show("slide", { direction: "left" }, 1000);
+
 
 }
 function rightPlayerTurn(output){
-  
+  console.log('right player ' + output);  
   //if output = ping - show ping img, add 1 point
   
   //if output = pong - show pong img, add 2 point
@@ -60,15 +130,18 @@ function rightPlayerTurn(output){
   //if output = number - show number
   
   // show right-ball img
-  // $(this).show("slide", { direction: "left" }, 1000);
+
 
 }
 
 
 function gamePlay(ping, pong, length) {
   var output;
+  // console.log('gamePlay called');
+  motionLeft(motionRight);
   //basic game loop
   for (i=1; i<=length; i++) {
+    // console.log('loop ' + i)
     //if-else to establish output value;
     if (i%ping===0 && i%pong===0) {
         output = 'ping-pong';
@@ -92,15 +165,19 @@ $(document).ready(function(){
 
     //get variables
     pingInput = parseInt($('#ping-input').val(), 10);
-    pongInput = parseInt($('pong-input').val(), 10);
-    gameLength = parseInt($('game-length').val(), 10);
+    // console.log(pingInput);
+    pongInput = parseInt($('#pong-input').val(), 10);
+    // console.log(pongInput);
+    gameLength = parseInt($('#game-length').val(), 10);
+    // console.log(gameLength);
 
     //switch divs
-    $('#start').fadeOut( 400 );
-    $('#game-area').delay( 600 ).fadeIn( 400 );
+    $('#start').fadeOut(400);
+    $('#game-area').delay(600).fadeIn(400);
 
-    //countdown
-    countDown(motionLeft);
+    //function calls
+    // countDown(gamePlay);
+    countDown(gamePlay);
    
 
     
