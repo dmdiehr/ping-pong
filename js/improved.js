378e6162f76852c004b915765b8f9f42988c8002
dmdiehr <ddiehr@yahoo.com>
2015-10-19 17:01:14 -0700

    Replace game loop with recursive callback model. Updated html divs. Nearly finished with leftPlayerturn function.

--- js/improved.js
+++ js/improved.js
@@ -4,7 +4,12 @@ var pongInput;
 var gameLength;
 var leftScore = 0;
 var rightScore = 0;
-var counter = 0;
+var leftScoreDisplay;
+var rightScoreDisplay;
+var counter = 1;
+var ping = false;
+var pong = false;
+var pingPong = false;
 
 
 ////////////////////////functions to control div animation//////////////////////////////////////
@@ -16,7 +21,7 @@ function countDown(callback){
         $('#countdown-start').fadeIn(1000).delay(500).fadeOut(100, function(){
           if (callback){
             // console.log('callback exists');
-            callback(pingInput, pongInput, gameLength);
+            callback(rightPlayerTurn);
           }
         });//countdown-start ending
       });//countdown1 ending
@@ -27,10 +32,10 @@ function countDown(callback){
 ////////////////////////////////////////center div////////////////////////////////////////////////
 
 function motionLeft(callback){
+  console.log('motionLeft ' + counter);
   $('#left-motion').fadeIn(500).delay(500).fadeOut(500, function(){
     if (callback){
       counter++;
-      console.log('motionLeft ' + counter)
       if (counter<gameLength){
         callback(motionLeft);
       } else {
@@ -40,10 +45,10 @@ function motionLeft(callback){
   });
 }
 function motionRight(callback){
+  console.log('motionRight '+ counter);
   $('#right-motion').fadeIn( 500 ).delay(500).fadeOut(500, function(){
     if (callback){
       counter++;
-      console.log('motionRight '+ counter)
       if (counter<gameLength){
         callback(motionRight);
       } else {
@@ -52,10 +57,6 @@ function motionRight(callback){
     }
   });
 }
-
-// function centerAnimationLoop(length){
-//     motionLeft(motionRight);
-//   }
 
 //////////////////////////////////left div/////////////////////////////////////////////////////
 
@@ -103,9 +104,59 @@ function righttPongPong(callback){
   });
 }
 
+////////////////////////////////////Game Play/////////////////////////////////////////////////
 
-function leftPlayerTurn(output){
-  console.log('left player ' + output);
+function leftPlayerTurn(callback){
+  console.log('left player, turn '+ counter);
+  // console.log(pingInput);
+  // console.log(pongInput);
+  // console.log(gameLength);
+
+                          //Establish Booleans//
+
+  ping = false;
+  pong = false;
+  pingPong = false;
+
+  if (counter%pingInput===0 && counter%pongInput===0) {
+        pingPong = true;
+      } else if (counter%pongInput===0) {
+        pong = true;
+      } else if (counter%pingInput===0) {
+        ping = true;
+      }//end of if-else to set booleans
+
+                      //Animatons, Callbacks, and Scoring//
+
+  if (pingPong){
+    leftScore+=5;
+    $('#left-score').text(leftScore);
+    $('#left-ping-pong').fadeIn(500).delay(500).fadeOut(500);
+  } else if (pong){
+    leftScore+=2;
+    $('#left-score').text(leftScore);
+    $('#left-pong').fadeIn(500).delay(500).fadeOut(500);
+  } else if (ping){
+    leftScore+=1;
+    $('#left-score').text(leftScore);
+    $('#left-ping').fadeIn(500).delay(500).fadeOut(500);
+  } else {
+    $('#left-number').text(counter);
+    $('#left-number').fadeIn(500).delay(500).fadeOut(500);
+
+  }
+
+  $('#left-motion').fadeIn(500).delay(500).fadeOut(500, function(){
+    if (callback){
+      counter++;
+      if (counter<gameLength){
+        callback(leftPlayerTurn);
+      } else {
+        callback();
+      }
+    }
+  });
+}
   
   //if output = ping - show ping img, add 1 point
 
@@ -117,22 +168,47 @@ function leftPlayerTurn(output){
   
   // show left-ball img
 
+function rightPlayerTurn(callback){
+  console.log('right player turn ' + counter);
 
+                          //Establish Booleans//
+  ping = false;
+  pong = false;
+  pingPong = false;
+
+  if (counter%pingInput===0 && counter%pongInput===0) {
+        pingPong = true;
+      } else if (counter%pongInput===0) {
+        pong = true;
+      } else if (counter%pingInput===0) {
+        ping = true;
+      }//end of if-else to set booleans
+
+                      //Animatons, Callbacks, and Scoring//
+
+  $('#right-motion').fadeIn(500).delay(500).fadeOut(500, function(){
+    if (callback){
+      counter++;
+      if (counter<gameLength){
+        callback(rightPlayerTurn);
+      } else {
+        callback();
+      }
+    }
+  });
 }
-function rightPlayerTurn(output){
-  console.log('right player ' + output);  
-  //if output = ping - show ping img, add 1 point
+  // if output = ping - show ping img, add 1 point
   
-  //if output = pong - show pong img, add 2 point
+  // if output = pong - show pong img, add 2 point
   
-  //if output = ping-pong - show ping-pong img, add 5 point
+  // if output = ping-pong - show ping-pong img, add 5 point
   
-  //if output = number - show number
+  // if output = number - show number
   
   // show right-ball img
 
 
-}
+
 
 
 function gamePlay(ping, pong, length) {
@@ -140,22 +216,22 @@ function gamePlay(ping, pong, length) {
   // console.log('gamePlay called');
   motionLeft(motionRight);
   //basic game loop
-  for (i=1; i<=length; i++) {
-    // console.log('loop ' + i)
-    //if-else to establish output value;
-    if (i%ping===0 && i%pong===0) {
-        output = 'ping-pong';
-      } else if (i%pong===0) {
-        output = 'pong';
-      } else if (i%ping===0) {
-        output = 'ping';
-      } else {
-        output = i;
-    }//end of if-else to set output
+  // for (i=1; i<=length; i++) {
+  //   // console.log('loop ' + i)
+  //   //if-else to establish output value;
+  //   if (i%ping===0 && i%pong===0) {
+  //       output = 'ping-pong';
+  //     } else if (i%pong===0) {
+  //       output = 'pong';
+  //     } else if (i%ping===0) {
+  //       output = 'ping';
+  //     } else {
+  //       output = i;
+  //   }//end of if-else to set output
 
-    if (i%2!==0) {leftPlayerTurn(output);} //if odd then it's the left players turn
-    if (i%2===0) {rightPlayerTurn(output);} // if even then it's the right players turn
-  }//end of for-loop
+  //   if (i%2!==0) {leftPlayerTurn(output);} //if odd then it's the left players turn
+  //   if (i%2===0) {rightPlayerTurn(output);} // if even then it's the right players turn
+  // }//end of for-loop
 }
 
 
@@ -177,7 +253,7 @@ $(document).ready(function(){
 
     //function calls
     // countDown(gamePlay);
-    countDown(gamePlay);
+    countDown(leftPlayerTurn);
    
 
     
